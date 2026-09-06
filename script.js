06a788475c089814dfaf969a4c3ce11ad7509a80
mubvute66 <mubvute66@gmail.com>
2026-09-06 10:27:31 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,23 @@
+document.addEventListener("DOMContentLoaded", function () {
+
+    const menuBtn = document.getElementById("menuBtn");
+    const navLinks = document.getElementById("navLinks");
+
+    if (menuBtn && navLinks) {
+
+        menuBtn.addEventListener("click", function () {
+            navLinks.classList.toggle("active");
+        });
+
+        const links = navLinks.querySelectorAll("a");
+
+        links.forEach(function (link) {
+
+            link.addEventListener("click", function () {
+                navLinks.classList.remove("active");
+            });
+
+        });
+    }
+
+});
